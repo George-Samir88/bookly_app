846fd82280255f9fa0256e9c3479bf469f287c7a
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-08-06 20:31:09 +0300

    adding sliding text animation to splash view

--- lib/features/home/presentation/views/widgets/splash_view_body.dart
+++ lib/features/home/presentation/views/widgets/splash_view_body.dart
@@ -1,24 +1,51 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/cupertino.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slideAnimation = Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+    // slideAnimation.addListener(() {
+    //   setState(() {});
+    // }); //replace it with animated builder
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children:
-      [
+      children: [
         Image.asset(CustomAssets.logo),
         const SizedBox(
           height: 5.0,
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        ),
+        SlidingText(slideAnimation: slideAnimation),
       ],
     );
   }
