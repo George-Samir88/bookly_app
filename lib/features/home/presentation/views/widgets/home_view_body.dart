cf1339dee19cece1670f33dc95397381c686d3e2
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-08-08 22:42:59 +0300

    best seller item of list view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view.dart';
@@ -26,8 +27,40 @@ class HomeViewBody extends StatelessWidget {
               'Best Seller',
               style: Styles.titleMedium,
             ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            const BestSellerItemOfListView(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BestSellerItemOfListView extends StatelessWidget {
+  const BestSellerItemOfListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125.0,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(16),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(CustomAssets.test),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
