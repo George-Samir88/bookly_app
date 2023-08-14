2e2a7305e71e2ff8d387dd5758972edad63e3fdb
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-08-14 23:44:02 +0300

    add either class

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,4 +1,9 @@
-abstract class HomeRepo{
-  fetchFeaturedBooks();
-  fetchBestSellerBooks();
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 }
