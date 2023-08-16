1a87d28fadca1ec21d6ac0a7a57d8b6b71ce100f
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-08-16 13:50:54 +0300

    implement fetch featured books method

--- lib/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
@@ -1,12 +1,23 @@
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repo/home_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
 
   static FeaturedBooksCubit getOb(context) =>
       BlocProvider.of<FeaturedBooksCubit>(context);
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksGetFailureState(failure.toString()));
+    }, (books) {
+      emit(FeaturedBooksGetSuccessState(books));
+    });
+  }
 }
